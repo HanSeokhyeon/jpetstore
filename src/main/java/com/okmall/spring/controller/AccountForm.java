0024a40c2382b23070fd770f7f19f04539a13a4d
hanseokhyeon <sah0322@naver.com>
2021-02-03 16:11:57 +0900

    feat: first init

---
+++ src/main/java/com/okmall/spring/controller/AccountForm.java
@@ -0,0 +1,39 @@
+package com.okmall.spring.controller;
+
+import com.okmall.spring.component.validation.RepeatedField;
+import com.okmall.spring.domain.Account;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RepeatedField(field = "password")
+@Getter
+@Setter
+public class AccountForm {
+    @NotNull(groups = Create.class)
+    @Size(max = 40)
+    private String username;
+
+    @NotNull(groups = Create.class)
+    @Size(min = 8)
+    private String password;
+
+    private String repeatedPassword;
+
+    @NotNull
+    @Email
+    private String email;
+
+
+    Account toAccount() {
+        Account account = new Account();
+        BeanUtils.copyProperties(this, account);
+        return account;
+    }
+
+    interface Create {}
+}
---
+++ src/main/java/com/okmall/spring/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.okmall.spring.controller;
+
+import com.okmall.spring.component.message.Messages;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/login")
+@Controller
+public class AuthController {
+
+    @GetMapping
+    public String loginForm() {
+        return "auth/login";
+    }
+
+    @GetMapping(params = "error")
+    public String loginError(Model model) {
+        model.addAttribute(new Messages().error("Credentials is invalid"));
+        return loginForm();
+    }
+}
